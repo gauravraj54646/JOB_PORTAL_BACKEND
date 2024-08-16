a19b8c608450672c745f9253f35c881e9d526808
gauravraj54646 <123385178+gauravraj54646@users.noreply.github.com>
2024-08-16 21:51:52 +0530

    Update app.js
    change to process.env.FRONTENDURL

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ config({ path: "./config/config.env" });
 
 app.use(   //for frontend and backend connectection
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [https://job-portal-frontend-khaki-mu.vercel.app],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
